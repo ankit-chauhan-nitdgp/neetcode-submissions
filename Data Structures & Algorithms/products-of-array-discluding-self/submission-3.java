420e08bda2d30f17258b2596da35f2240118ff4c
Ankit Chauhan <chauhana595@gmail.com>
2026-04-29 19:24:00 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] prefixProduct = new int[n];
+        int temp = 1;
+
+        for(int i = 0; i<n; i++){
+                prefixProduct[i] = temp;
+                temp = prefixProduct[i]*nums[i];
+        }
+
+        temp = 1;
+
+        for(int i = n-1; i>=0 ; i--){
+                prefixProduct[i] = prefixProduct[i]*temp;
+                temp = temp*nums[i];
+        }
+        
+        return prefixProduct;
+    }
+}  
+
+
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int[] ans = new int[n];
+//         int[] prefixProduct = new int[n+1];
+//         int[] suffixProduct = new int[n+1];
+//         prefixProduct[0] = 1;
+//         suffixProduct[n-1] = 1;
+        
+
+//         int temp = 1;
+//         for(int i = 0; i<n; i++){
+//                 prefixProduct[i] = temp;
+//                 temp = prefixProduct[i]*nums[i];
+//         }
+//         temp = 1;
+//         for(int i = n-1; i>=0 ; i--){
+//                 suffixProduct[i] = temp;
+//                 temp = suffixProduct[i]*nums[i];
+//         }
+
+//         for(int i = 0; i<n; i++){
+//             ans[i] = prefixProduct[i]*suffixProduct[i];
+//         }
+
+//         return ans;
+//     }
+// }  
