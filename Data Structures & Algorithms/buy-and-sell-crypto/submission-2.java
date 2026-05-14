20ecb5bf54dd695e5034321ed8ddc7463946cfa7
Ankit Chauhan <chauhana595@gmail.com>
2026-05-14 11:30:46 +0530

    Add example input/output to maxProfit method
    Added example input and output in comments for clarity.

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -1,4 +1,8 @@
 class Solution {
+    /* Input: prices = [10,1,5,6,7,1]
+    Output: 6 
+    Exp: buy for 1 (day 2) sell for 7 (day 5); 
+    */
     public int maxProfit(int[] prices) {
         int ans = 0;
         int n = prices.length;
